edb2eeb887ed573a610160addf8a1050fef2a5b3
pzoli <papp.zoltan@integrity.hu>
2025-04-25 14:15:42 +0200

    testing send message

--- src/test/java/hu/infokristaly/messagingproducer/Homework4messagingproducerApplicationTests.java
+++ src/test/java/hu/infokristaly/messagingproducer/Homework4messagingproducerApplicationTests.java
@@ -1,13 +1,24 @@
 package hu.infokristaly.messagingproducer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest(classes = Homework4messagingproducerApplication.class)
 class Homework4messagingproducerApplicationTests {
 
+	@Autowired // Inject the ApplicationContext itself
+    private ApplicationContext context;
+	
 	@Test
 	void contextLoads() {
+		Homework4messagingproducerApplication bean = context.getBean(Homework4messagingproducerApplication.class);
+		assertNotNull(bean);
+		bean.main(new String[] {});
+		bean.sendMessagePeriodically();
 	}
 
 }
